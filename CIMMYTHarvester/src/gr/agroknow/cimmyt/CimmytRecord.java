c985bbc8158e7b829ad6108379a3047a668e8286
mihalispap <mihalispapak@gmail.com>
2016-03-10 13:06:03 +0200

    element parser first

--- CIMMYTHarvester/src/gr/agroknow/cimmyt/CimmytRecord.java
+++ CIMMYTHarvester/src/gr/agroknow/cimmyt/CimmytRecord.java
@@ -1,5 +1,6 @@
 package gr.agroknow.cimmyt;
 
+import java.util.List;
 import java.util.Vector;
 
 import org.jdom.Document;
@@ -21,15 +22,44 @@ public class CimmytRecord extends OAIRecord  {
         this.priCheckIdOnly();
         try {
             Namespace oains = Namespace.getNamespace((String)"oai", (String)"http://www.openarchives.org/OAI/2.0/");
+ 		   	Namespace dcns = Namespace.getNamespace((String)"dc", (String)"http://purl.org/dc/elements/1.1/");
+ 		   	Namespace oaidcns = Namespace.getNamespace((String)"oai_dc", (String)"http://www.openarchives.org/OAI/2.0/oai_dc/");
+ 		   
             Vector<Namespace> nsVector = new Vector<Namespace>();
             nsVector.add(oains);
+            nsVector.add(dcns);
+            nsVector.add(oaidcns);
             Element node = OaiUtil.getXpathNode("oai:metadata/*", nsVector, this.xmlRecord);
             if (node != null) {
                 node = (Element)node.clone();
                 node.detach();
                 ret = new Document(node).getRootElement();
-                
-                System.out.println(ret.getAttributeValue("dc:description"));
+
+
+                System.out.println("Root element :" 
+                        + ret.getName());
+
+                     Element classElement = ret;
+
+                     List<Element> resourceList = classElement.getChildren();
+                     System.out.println("----------------------------");
+
+                     for (int i = 0; i < resourceList.size(); i++) {    
+                        Element resource_tag = resourceList.get(i);
+                        System.out.println("\nCurrent Element:" 
+                           + resource_tag.getName());
+                        
+                        if(resource_tag.getName().equals("title"))
+                        	System.out.println("TITLE:"+resource_tag.getText());
+                        
+                        /*Attribute attribute =  student.getAttribute("rollno");
+                        System.out.println("Student roll no : " 
+                           + attribute.getValue() );*/
+                        /*System.out.println("First Name : " + student.getChild("firstname").getText());
+                        System.out.println("Last Name : "+ student.getChild("lastname").getText());
+                        System.out.println("Nick Name : "+ student.getChild("nickname").getText());
+                        System.out.println("Marks : "+ student.getChild("marks").getText());	*/            		
+                     }
             }
         }
         catch (JDOMException e) {
